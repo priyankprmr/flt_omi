300dc0351593b49e98e993d1c2ac35fc7c8e6564
Priyank <priyankprmr@gmail.com>
2025-12-29 21:03:25 +0530

    - auth screen ui done

---
+++ lib/core/constants/app_texts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class AppTexts {
+  const AppTexts._();
+
+  static const String signIn = 'Sign In';
+  static const String signUp = 'Sign Up';
+
+  static const String dontHaveAccount = 'Don\'t have an account? ';
+  static const String alreadyHaveAccount = 'Already have an account? ';
+}
--- lib/features/auth/presentation/screen/auth_screen.dart
+++ lib/features/auth/presentation/screen/auth_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flt_omi/core/constants/app_texts.dart';
 import 'package:flt_omi/core/utils/padding.dart';
+import 'package:flt_omi/features/auth/domain/auth_form_type.dart';
+import 'package:flt_omi/features/auth/presentation/providers/auth_form_provider.dart';
+import 'package:flt_omi/features/auth/presentation/widgets/signin_form.dart';
 import 'package:flt_omi/features/auth/presentation/widgets/signup_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,20 +12,86 @@ class AuthScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Form type from auth form type provider
+    final formType = ref.watch(authFormProvider);
+    
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up'), centerTitle: true),
+      appBar: AppBar(
+        title: switch (formType) {
+          AuthFormType.signin => const Text(AppTexts.signIn),
+          AuthFormType.signup => const Text(AppTexts.signUp),
+        },
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            const SignupForm().padding(
-              const EdgeInsetsGeometry.symmetric(vertical: 20.0),
-            ),
             const SizedBox(height: 20.0),
-            ElevatedButton(onPressed: () {}, child: const Text('Sign Up')),
+            // Sign In and Sign Up forms
+            switch (formType) {
+              AuthFormType.signin => const SigninForm(),
+              AuthFormType.signup => const SignupForm(),
+            },
+            const SizedBox(height: 20.0),
+            // Buttons
+            switch (formType) {
+              AuthFormType.signin => ElevatedButton(
+                  onPressed: () {}, child: const Text(AppTexts.signIn)),
+              AuthFormType.signup => ElevatedButton(
+                  onPressed: () {}, child: const Text(AppTexts.signUp)),
+            },
+            const SizedBox(height: 20.0),
+            // Rich text to toggle forms
+            Text.rich(
+              switch (formType) {
+                AuthFormType.signin => _formToggleText(
+                    context: context,
+                    prefixText: AppTexts.dontHaveAccount,
+                    suffixText: AppTexts.signUp,
+                    onTap: ref.read(authFormProvider.notifier).toggle,
+                  ),
+                AuthFormType.signup => _formToggleText(
+                    context: context,
+                    prefixText: AppTexts.alreadyHaveAccount,
+                    suffixText: AppTexts.signIn,
+                    onTap: ref.read(authFormProvider.notifier).toggle,
+                  ),
+              },
+            ),
           ],
         ).padding(const EdgeInsets.symmetric(horizontal: 16.0)),
       ),
+    );
+  }
+
+  TextSpan _formToggleText({
+    required BuildContext context,
+    required String prefixText,
+    required String suffixText,
+    required VoidCallback onTap,
+  }) {
+    return TextSpan(
+      text: prefixText,
+      style: Theme.of(context).textTheme.bodyLarge,
+      children: [
+        WidgetSpan(
+          alignment: PlaceholderAlignment.baseline,
+          baseline: TextBaseline.alphabetic,
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: onTap,
+              child: Text(
+                suffixText,
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
